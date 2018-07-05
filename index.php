cf4654a9e2e15093a618966eb3a53aedce680823
Fred the Web Guy <35805971+fredthewebguy@users.noreply.github.com>
2018-07-05 06:23:19 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,276 @@
+<!doctype html>
+
+	<head>
+
+		<meta charset="utf-8">
+		<meta http-equiv="x-ua-compatible" content="ie=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+		<meta name="robots" content="noindex,nofollow">
+		<meta name="googlebot" content="noindex,nofollow">
+
+		<title>PostingsPlus Sample</title>
+
+		<link rel="icon" type="image/png" sizes="16x16" href="/path/to/favicon-16x16.png">
+		<link rel="icon" type="image/png" sizes="32x32" href="/path/to/favicon-32x32.png">
+		<link rel="icon" type="image/png" sizes="96x96" href="/path/to/favicon-96x96.png">
+
+		<link rel="stylesheet" href="style.css">
+		<link rel="stylesheet" href="fontello.css">
+
+	</head>
+
+	<body>
+
+		<!-- -->
+		<div class="search-panel">
+			<form>
+				<span class="close-search">+</span>
+				<input type="text" placeholder="What are you looking for?" required>
+			</form>
+		</div><!-- # -->
+
+		<!-- begin -->
+		<header>
+
+			<a href="#" id="logo"><img src="images/logo.png" alt="Postings Plus"></a>
+
+			<!-- begin -->
+			<nav role="navigation">
+				<ul>
+					<li><a href="#">Blog</a></li>
+					<li><a href="#">Solutions</a></li>
+					<li><a href="#">About Us</a></li>
+					<li><a href="#">Job Seekers</a></li>
+					<li><a href="#">Employer Login</a></li>
+					<li><a href="#contact" id="questions">Contact Us</a></li>
+					<li><a href="#" id="toggleSearch"><i class="icon-search"></i></a></li>
+				</ul>
+			</nav><!-- # -->
+
+		</header><!-- # -->
+
+		<!-- begin -->
+		<div class="home-section banner">
+			<div>
+				<h1>The Talent Aquisition Specialists</h1>
+				<p>Outsourced Recruitment | Project Recruitment | Contingency Search</p>
+				<span class="button green"><a href="#">I'm a job seeker</a></span>
+				<span class="button"><a href="#">I'm an employer</a></span>
+			</div>
+		</div><!-- # -->
+
+		<!-- begin -->
+		<div class="home-section">
+
+			<h2>We connect employers and job seekers for lasting success</h2>
+
+			<p>Whether you are looking to fill a challenging role or staff multiple positions we offer tailored solutions to fit every budget.</p>
+
+			<div class="value-box">
+				<h3>Outsourced + Project Recruitment</h3>
+				<p>Ideal for your ongoing, seasonal, new location or timebound hiring needs.</p>
+				<ul>
+					<li><i class="icon-ok"></i>Showcase and market your brand to job seekers</li>
+					<li><i class="icon-ok"></i>Utilize top national, diversity specific and local job boards</li>
+					<li><i class="icon-ok"></i>Screen candidates based on your hiring criteria</li>
+					<li><i class="icon-ok"></i>Competitive pricing based on job type &amp; position level</li>
+				</ul>
+				<span class="button"><a href="#">Full features</a></span>
+			</div>
+
+			<div class="value-box">
+				<h3>Job Marketing</h3>
+				<p>Ideal for companies looking to increase applicant flow and support internal recruiters.</p>
+				<ul>
+					<li><i class="icon-ok"></i>Showcase and market your brand to job seekers</li>
+					<li><i class="icon-ok"></i>Utilize top national, diversity specific and local job boards, and everage top social media sites</li>
+					<li><i class="icon-ok"></i>Manage job marketing</li>
+					<li><i class="icon-ok"></i>Dedicated Job Analyst, Web portal &amp; Applicant Tracking System Included</li>
+				</ul>
+				<span class="button"><a href="#">Full features</a></span>
+			</div>
+
+			<div class="value-box">
+				<h3>Applicant Screening + Interviewing</h3>
+				<p>Ideal for companies with high applicant flow.</p>
+				<ul>
+					<li><i class="icon-ok"></i>Screen &amp; shortlist candidates based on your hiring criteria</li>
+					<li><i class="icon-ok"></i>Phone screening and 24-hour criminal checks</li>
+					<li><i class="icon-ok"></i>Reference checks and drug screening</li>
+					<li><i class="icon-ok"></i>No contracts necessary, competitive flat fee pricing, and volume discounts</li>
+				</ul>
+				<span class="button"><a href="#">Full features</a></span>
+			</div>
+
+			<div class="value-box">
+				<h3>Job Seekers</h3>
+				<p>We help ease the pressure of your job search with amazing service.</p>
+				<ul>
+					<li><i class="icon-ok"></i>Dedicated, accessible Agents</li>
+					<li><i class="icon-ok"></i>Thorough understanding of your field and helping you find the best fit</li>
+					<li><i class="icon-ok"></i>Help with preparing you to nail your next interview</li>
+					<li><i class="icon-ok"></i>Access to resources and help in your career success</li>
+				</ul>
+				<span class="button"><a href="#">View open positions</a></span>
+			</div>
+
+			<br class="clearfix">
+
+			<span class="button bigger"><a href="#contact">Let's get to work together!</a></span>
+
+		</div><!-- # -->
+
+		<!-- begin -->
+		<div class="home-section blue" id="testimonials">
+
+			<h2>What our clients are saying</h2>
+
+			<div class="quote">
+				<img src="images/quote2.jpg" alt="">
+				<p>"Quote from a job seeker goes here, detailing how dedicated and personalized the service is and how it's helped them make the next step in their career."</p>
+				<span>Name Here<br>their title</span>
+			</div>
+
+			<div class="quote">
+				<img src="images/quote3.jpg" alt="">
+				<p>"Quote from an employer goes here, detailing how PostingsPlus handled every step of the recruitment process with throughness and helped them find the perfect talent for their organization."</p>
+				<span>Name Here<br>their title and company</span>
+			</div>
+
+			<div class="quote">
+				<img src="images/quote1.jpg" alt="">
+				<p>"Quote from a job seeker goes here, detailing how dedicated and personalized the service is and how it's helped them make the next step in their career."</p>
+				<span>Name Here<br>their title</span>
+			</div>
+
+		</div><!-- # -->
+
+		<!-- begin -->
+		<div class="home-section" id="about">
+
+			<h2>Reasons to work with us</h2>
+
+			<p>We take great pride in the brands we support and express that pride to every candidate reached.</p>
+
+			<div>
+				<h3>Branding + Marketing</h3>
+				<p>When utilizing PostingsPlus for either outsourced or project recruitment we showcase your company in the right places. All positions are sponsored and managed with your branding on top social media sites and leading job boards.</p>
+			</div>
+
+			<div>
+				<h3>Sourcing + Recruiting</h3>
+				<p>Industry-specific and dedicated in-house recruiters. We leverage top resume databases and social media sources to engage active and passive candidates.</p>
+			</div>
+
+			<div>
+				<h3>Pricing Strategy</h3>
+				<p>We strive to offer flexible solutions that fit your budget. By creating strategic partnerships with our vendors we are able to offer the most competitive pricing available.</p>
+			</div>
+
+			<div>
+				<h3>Headhunting</h3>
+				<p>When utilizing PostingsPlus for a Contingency Search we strictly headhunt talent. By leveraging our extensive talent network, social media reach, and cold calling abilities we engage the best passive talent for your brand.</p>
+			</div>
+
+			<div>
+				<h3>Our Values</h3>
+				<p>We strive to create the best work environment and culture for our employees. We believe that if we take care of our employees, they will take care of our candidates and customers.</p>
+			</div>
+
+			<br class="clearfix">
+
+			<span class="button bigger"><a href="#contact">Learn more about us</a></span>
+
+		</div><!-- # -->
+
+		<!-- begin -->
+		<div class="home-section grey" id="recent-articles">
+
+			<h2>Recent Insights</h2>
+
+			<article>
+				<a href="#"><img src="images/blog4.jpg" alt=""></a>
+				<h3><a href="#">Post title goes here and takes you to the article</a></h3>
+				<span class="post-date">
+					<i class="icon-calendar"></i>Friday, April 13, 2018
+					<i class="icon-chat"></i><a href="#">0 comments</a>
+				</span>
+			</article>
+
+			<article>
+				<a href="#"><img src="images/blog1.jpg" alt=""></a>
+				<h3><a href="#">Post title goes here and takes you to the article</a></h3>
+				<span class="post-date">
+					<i class="icon-calendar"></i>Friday, April 13, 2018
+					<i class="icon-chat"></i><a href="#">0 comments</a>
+				</span>
+			</article>
+
+			<article>
+				<a href="#"><img src="images/blog2.jpg" alt=""></a>
+				<h3><a href="#">Post title goes here and takes you to the article</a></h3>
+				<span class="post-date">
+					<i class="icon-calendar"></i>Friday, April 13, 2018
+					<i class="icon-chat"></i><a href="#">0 comments</a>
+				</span>
+			</article>
+
+			<article>
+				<a href="#"><img src="images/blog3.jpg" alt=""></a>
+				<h3><a href="#">Post title goes here and takes you to the article</a></h3>
+				<span class="post-date">
+					<i class="icon-calendar"></i>Friday, April 13, 2018
+					<i class="icon-chat"></i><a href="#">0 comments</a>
+				</span>
+			</article>
+
+			<br class="clearfix">
+
+			<span class="button"><a href="#">View all articles</a></span>
+
+		</div><!-- # -->
+
+		<!-- begin -->
+		<div class="home-section" id="contact">
+
+			<section>
+				<h2>Contact Us</h2>
+				<p>If you’re considering PostingsPlus or just want more information, we’re happy to help. Fill out the form or call us and we’ll have a member of our team follow up with you right away.</p>
+				<p><strong>1 888 535 PLUS (7587)</strong><br><a href="mailto:info@postingsplus.com">info@postingsplus.com</a></p>
+				<a href="#"><i class="icon-twitter-bird"></i></a>
+				<a href="#"><i class="icon-linkedin"></i></a>
+				<a href="#"><i class="icon-facebook"></i></a>
+				<a href="#"><i class="icon-youtube"></i></a>
+				<a href="#"><i class="icon-mail"></i></a>
+			</section>
+
+			<section>
+				<form>
+					<label>Your name</label>
+					<input type="text" placeholder="First/last name" required>
+					<label>Your email</label>
+					<input type="text" placeholder="me@mymailhost.com" required>
+					<label>How can we help you?</label>
+					<textarea required></textarea>
+					<input type="submit" value="Send my message">
+				</form>
+			</section>
+
+		</div><!-- # -->
+
+		<!-- begin -->
+		<footer>
+			&copy;2018 PostingsPlus Corp.
+		</footer><!-- # -->
+
+		<div id="back-top"><a href="#"><i class="icon-up-open"></i></a><div>
+
+		<!-- js -->
+		<script type="text/javascript" src="js/jquery.js"></script>
+		<script type="text/javascript" src="js/pace.min.js"></script>
+		<script type="text/javascript" src="js/scripts.js"></script>
+
+	</body>
+
+</html>
